8ab6aef963a4c1fc63c1cbce82b5c72a9521ecb1
Steffen Moeller <moeller@debian.org>
2024-10-02 18:31:19 +0200

    Added now shared progress.{cpp,h}

---
+++ progress.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <unordered_map>
+#include <filesystem> // test and creating for directories
+#include <algorithm> // For removing brackets, replace
+#include <iomanip>   // For formatted output / setting precision
+#include <chrono>    // For progress timing
+#include <getopt.h>  // For GNU Getopt
+#include <math.h>
+
+/** \brief Function to display the progress bar
+ */
+void displayProgressBar(const float& progress) {
+    int barWidth = 75;  // Width of the progress bar
+    std::cout << "[";
+    int pos = barWidth * progress;
+    for (int i = 0; i < barWidth; ++i) {
+        if (i < pos) std::cout << "=";
+        else if (i == pos) std::cout << ">";
+        else std::cout << " ";
+    }
+    std::cout << "] " << std::fixed << std::setprecision(3) << progress * 100.0 << " %\r";
+    std::cout.flush();
+}
---
+++ progress.h
@@ -0,0 +1,6 @@
+#ifndef PROGRESS_H
+#define PROGRESS_H
+
+void displayProgressBar(const float& progress);
+
+#endif
