e32ac1db7a60a6a7310dfba38420222e7a1cdc08
Steffen Moeller <steffen_moeller@gmx.de>
2025-01-13 12:23:50 +0100

    feature: scripts/run CHR variable

--- scripts/run_what_is_missing.sh
+++ scripts/run_what_is_missing.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 set -e
 
-for i in $(make echo_bidirect)
+if [ -z "$CHR" ]; then
+    export CHR=unset
+    echo "E: Set 'CHR' environment variable."
+    exit 1
+fi
+
+mkdir -p output_Chr$CHR
+
+for i in $(make CHR=$CHR echo_bidirect)
 do
-    f="output_Chr1/$i"
+    f="output_Chr$CHR/$i"
     if [ -f "$f" ]; then
         # echo "I: Found $i - skipped"
-        echo 
+        echo -n
     else
         # echo "I: Need to make $f"
         # echo make $f
