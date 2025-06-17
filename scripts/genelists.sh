e58ef9b740a0c65936a8fa4374bc310a39aa5552
Steffen Moeller <moeller@debian.org>
2025-06-17 22:17:38 +0200

    analyze_matrix: Volcano-Plots

--- scripts/genelists.sh
+++ scripts/genelists.sh
@@ -10,7 +10,8 @@ mkdir -p "$ofolder"
 
 URL_prefix="https://www.gsea-msigdb.org/gsea/msigdb/human/download_geneset.jsp?geneSetName="
 URL_postfix="&fileType=TSV"
-lists="HALLMARK_EPITHELIAL_MESENCHYMAL_TRANSITION HALLMARK_WNT_BETA_CATENIN_SIGNALING HALLMARK_ANGIOGENESIS KEGG_MELANOMA REACTOME_NEURONAL_SYSTEM Nico_Analysis_DN_20250508 Nico_Analysis_TA_20250508 Nico_Analysis_TAandDN_20250508"
+lists="HALLMARK_EPITHELIAL_MESENCHYMAL_TRANSITION HALLMARK_WNT_BETA_CATENIN_SIGNALING HALLMARK_ANGIOGENESIS KEGG_MELANOMA REACTOME_NEURONAL_SYSTEM Nico_Analysis_DN_20250508 Nico_Analysis_TA_20250508 Nico_Analysis_TAandDN_20250508 Nico_Analysis_DN_20250618 Nico_Analysis_DN_Methup_20250618 Nico_Analysis_TA_20250618 Nico_Analysis_TA_MethUp_20250618"
+
 for list in $lists; do
     echo "$list"
 
@@ -27,7 +28,12 @@ for list in $lists; do
         echo "I: $list.promoter.bed and $list.transcript.bed already exist - skipping"
         continue
     else
-        genes=$(grep GENE_SYMBOLS "$fname" | cut -f2 | grep -v "^GENE_SYMBOL")
+        #echo "D: Grepping genes"
+        if ! genes=$(grep GENE_SYMBOLS "$fname" | cut -f2 | grep -v "^GENE_SYMBOL"); then
+            echo "E: Could not grep for GENE_SYMBOLS, check if a true tab is separating the genes from the row name."
+            exit 1
+        fi
+        #echo "D: Grepping genes found $genes"
         ./gtf_file_region_retrieval -c 1 -n "$genes" -f promoter > "$ofolder/$list.promoter.bed"
         ./gtf_file_region_retrieval -c 1 -n "$genes" > "$ofolder/$list.transcript.bed"
     fi
