6e1bbf9610f8529dde26c431b6f985d84c380ce4
Steffen Moeller <steffen_moeller@gmx.de>
2025-10-22 13:58:25 +0200

    scripts/bedtools_map_serial.sh: explicit 0 for missing data

--- scripts/bedtools_map_serial.sh
+++ scripts/bedtools_map_serial.sh
@@ -11,7 +11,7 @@ regions="$1"
 shift
 
 output="mapped_max_step1.bed"
-bedtools map -a "$regions" -b "$1" -o max -c 5 > "$output"
+bedtools map -a "$regions" -b "$1" -null 0 -o max -c 5 > "$output"
 echo "Mapped $1 to $output"
 shift
 
@@ -20,8 +20,8 @@ while [ "$#" -gt 0 ]; do
     prev_output="$output"
     output="mapped_max_step${step}.bed"
     # Append new column to previous output
-    bedtools map -a "$prev_output" -b "$1" -o max -c 5 | \
-        paste "$prev_output" - | cut -f1-$(awk '{print NF}' "$prev_output" | head -1),$(awk '{print NF+1}' "$prev_output" | head -1) > "$output"
+    bedtools map -a "$prev_output" -b "$1" -null 0 -o max -c 5 > "$output"
+    #paste "$prev_output" - | cut -f1-$(awk '{print NF}' "$prev_output" | head -1),$(awk '{print NF+1}' "$prev_output" | head -1) > "$output"
     echo "Mapped $1 to $output"
     shift
     step=$((step+1))
